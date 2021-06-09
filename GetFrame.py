41896d06dce34b26f893c521d517bcadf620fccb
Ivam Novais <ivamnovaisivam@gmail.com>
2021-06-09 11:11:53 -0300

    coreeçoes da ultima atualização

--- GetFrame.py
+++
@@ -1,9 +0,0 @@
-import GetVideo
-
-def GetFrames(dir, inicio, fim):
-  try:
-    imagens = GetVideo.capturaDir(inicio,fim,dir)
-  except:
-    print(f'erro ao carregar')
-    return False
-  return imagens
