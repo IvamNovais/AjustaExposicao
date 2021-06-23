286d0e691fc173f38d9708b70ce82cfd95825289
IvamNovais <72990399+IvamNovais@users.noreply.github.com>
2021-06-22 23:38:11 -0300

    Update AjustaExposicao.py

--- AjustaExposicao.py
+++ AjustaExposicao.py
@@ -2,10 +2,6 @@ from skimage.filters import threshold_yen
 from skimage.exposure import rescale_intensity
 import cv2
 
-def Expo(img):
-    yen_threshold = threshold_yen(img)
-    bright = rescale_intensity(img, (0, yen_threshold), (0, 255))
-    return bright
 def acertaBrilho (image):
     img = image
     yen_threshold = threshold_yen(img) 
